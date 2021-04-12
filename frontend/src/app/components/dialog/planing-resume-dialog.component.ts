33b013b9207ec12e85129932f33669694a3e2282
thiago-you <thiago.youx@gmail.com>
2021-04-12 01:21:06 -0300

    add basic layout on planning resume

--- frontend/src/app/components/dialog/planing-resume-dialog.component.ts
+++ frontend/src/app/components/dialog/planing-resume-dialog.component.ts
@@ -1,16 +1,37 @@
-import { MatDialogRef } from '@angular/material/dialog';
-import { Component } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
     selector: 'planning-resume-dialog',
     templateUrl: 'planning-resume-dialog.component.html',
     styleUrls: ['./base-dialog.component.scss']
 })
-export class PlanningResumeDialogComponent {
+export class PlanningResumeDialogComponent implements OnInit {
 
-    constructor(private dialogRef: MatDialogRef<PlanningResumeDialogComponent>) {}
+    items: any;
+
+    constructor(
+        private dialogRef: MatDialogRef<PlanningResumeDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) {
+        this.items = [];
+    }
+
+    ngOnInit() {
+        this.items = this.data.items || [];
+    }
 
     close() {
         this.dialogRef.close(true);
     }
+
+    getScore(value: string = ''): string {
+        let score = value || '';
+    
+        if (value == 'coffee') {
+          score = '☕';
+        }
+    
+        return score;    
+      }
 }
